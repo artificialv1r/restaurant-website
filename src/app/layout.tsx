02163a98646516c925c304c3854c186fca7f09fd
Stefan Gospic <stefan.gospic.1@gmail.com>
2025-04-15 18:37:23 +0200

    Add Menu Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col h-screen w-screen justify-center items-center overflow-hidden md:bg-[url('/assets/images/stefangospic_premium_baked_beef_stake_on_a_dark_plate_with_cl_1cee20bd-5a36-4781-88e1-90b9d02f78f5_2.webp')] md:bg-cover md:bg-center md:bg-no-repeat text-black">
-        <div className="w-[80dvw] h-[80dvh]">
+        <div className="w-[90dvw] h-[90dvh]">
         {children}
         </div>
       </body>
