d1a1ff28cee3e225aaa560ecda87aa4278ccabd1
pc <stefan.gospic.1@gmail.com>
2025-04-17 21:18:04 +0200

    Fix Mobile Responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
         <div className="h-[40dvh] md:h-full  md:w-1/2 p-[20px] bg-[url('/assets/images/stefangospic_premium_baked_beef_stake_on_a_dark_plate_with_so_a553d122-40f8-436e-a0ff-7f7c76dd2742_1.webp')] bg-cover bg-center bg-no-repeat rounded-[20px]">
           <Navbar />
         </div>
-        <div className="h-full md:w-1/2 grid grid-cols-1 gap-[10px] p-[20px] bg-linear-to-b from-[#0C0C0C] via-[#040303] to-[#0C0C0C] text-center justify-center rounded-[20px]">
+        <div className="h-fit flex flex-col gap-[20px] p-[20px] bg-linear-to-b from-[#0C0C0C] via-[#040303] to-[#0C0C0C] text-center justify-evenly rounded-[20px]">
           <h1 className="text-[24pt]">
             Premium Beef. Perfectly Crafted. Unforgettable Flavor.
           </h1>
